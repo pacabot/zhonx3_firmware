d38efcfae8c35e971169f9fc8c1348652a6c22dc
Colin Roubaud <pacabot.RCn@gmail.com>
2015-02-04 19:13:15 +0100

    ajout du fichier menu (oublier dans le commit précédent)
    Signed-off-by: Colin Roubaud <pacabot.RCn@gmail.com>

---
+++ Inc/peripherals/display/menu.h
@@ -0,0 +1,29 @@
+/*
+ * menu_colin.h
+ *
+ *  Created on: 4 déc. 2014
+ *      Author: colin
+ */
+
+#ifndef MENU_COLIN_H_
+#define MENU_COLIN_H_
+
+#define MAX_LINE_SCREEN 5
+typedef struct {
+char *name;
+char type;
+int (*param)(void);
+}lineItem;
+
+typedef struct{
+		char *name;
+		lineItem line[20];
+}menuItem;
+int menu(menuItem);
+void menu_animate(unsigned char y, unsigned char max_y);
+void affiche_menu(menuItem menu,int first_line);
+void anti_rebonds ();
+int modify_bool_param( char *param_name, unsigned char *param);
+int modify_long_param(char *param_name,unsigned long *param);
+
+#endif /* MENU_COLIN_H_ */
