e83f2071e59183db7c65492d142cfbb522cec682
patrick reybaud <pacabot.prd@gmail.com>
2015-03-06 23:07:55 +0100

    Add PID speed control

---
+++ Inc/peripherals/callback/user_it_callback.h
@@ -0,0 +1,19 @@
+/**************************************************************************/
+/*!
+    @file     Callback.h
+    @author   PLF Pacabot.com
+    @date     03 August 2014
+    @version  0.00
+*/
+/**************************************************************************/
+#ifndef __CALLBACK_H__
+#define __CALLBACK_H__
+
+/* Module Identifier */
+#define CALLBACK_MODULE_ID  2
+
+/* Error codes */
+#define CALLBACK_E_SUCCESS  0
+#define CALLBACK_E_ERROR    MAKE_ERROR(BLUETOOTH_DRIVER_MODULE_ID, 1)
+
+#endif
