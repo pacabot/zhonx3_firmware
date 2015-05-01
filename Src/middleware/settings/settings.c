c4305028287292a5abb4a6334e7bd2a3eb1c374e
Colin Roubaud <pacabot.RCn@gmail.com>
2015-05-01 18:11:21 +0200

    add missing files add Inc/middleware/settings/setting.h and Src/middleware/settings/settings.c

---
+++ Src/middleware/settings/settings.c
@@ -0,0 +1,13 @@
+/*
+ * settings.c
+ *
+ *  Created on: 3 avr. 2015
+ *      Author: Colin
+ */
+#include "middleware/settings/setting.h"
+#include "peripherals/eeprom/24lc64.h"
+int settingsInit (void)
+{
+
+	return SETTING_MODULE_E_SUCCESS;
+}
