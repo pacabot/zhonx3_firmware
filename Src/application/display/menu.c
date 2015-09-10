c63325f6bf174afd006441328e3d5f96de1f27c1
Colin Roubaud <pacabot.RCn@gmail.com>
2015-09-10 22:01:40 +0200

    update menu.c

--- Src/application/display/menu.c
+++ Src/application/display/menu.c
@@ -43,7 +43,6 @@ extern void motorsTest ();
 extern void lineSensorsTest ();
 extern void lineFollower();
 extern void mainControlTest ();
-extern int 	wallSensorsCalibration (void);
 extern int 	lineSensorsCalibration (void);
 extern void testTelemeterDistance();
 extern void maze();
@@ -52,6 +51,7 @@ extern void followWallTest(void);
 extern void followLineTest(void);
 extern void rotateTest(void);
 extern void curveRotateTest(void);
+extern void telemeterFrontCalibration(void);
 
 /*
  * to create a new menu you have to create a new variable of type "const menuItem" like this :
@@ -111,7 +111,7 @@ const menuItem parameters_menu=
 {
 		"Parameters menu",
 		{
-				{"calibration",'f',(void*)wallSensorsCalibration}
+//				{"calibration",'f',(void*)wallSensorsCalibration}
 		}
 };
 const menuItem peripheral_test_menu=
@@ -159,6 +159,7 @@ const menuItem mainMenu =
 		"ZHONX III dark   V0.2",
 #endif
 		{
+				{"telemeters calibration",'f',		(void*)telemeterFrontCalibration},
 				{"Maze menu",'m',			(void*)&maze_menu},
 				{"Unit tests",'m',			(void*)&tests_menu},
 				{"Control menu",'m',		(void*)&control_menu},
